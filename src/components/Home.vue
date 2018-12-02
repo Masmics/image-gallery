2cf80a2eba30578fc04a8acf4ddd66f9917cd9bc
Christina T. Herlocker <cherlocker3113@gmail.com>
2018-12-02 11:25:03 -0800

    pushing branch

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,14 +1,16 @@
 <template>
-    <section>
-        <hr>
-        <h1>Fabulous Dogs</h1>
-        <hr>
+    <section id="home">
+        <span id="homey">
+            <hr>
+            <h1>Fabulous Dogs</h1>
+            <hr>
+        </span>
+        <img id="yellowdogs" src="http://intl.petsafe.net/intl/blog/wp-content/uploads/2013/05/lots-of-happy-dogs.jpg">
     </section>
 </template>
 
 <script>
 export default {
-
 };
 </script>
 
@@ -16,10 +18,19 @@ export default {
 h1 {
     text-align: center;
     font-size: 52px;
+    background-color: #bedb39
+    z-index=  +2;
 }
 section {
     width: 100vw;
     height: 100vh;
     background-repeat: no-repeat;
+}
+#home {
+    background-color: #ffe11a
+}
+#yellowdogs {
+    height: 100%;
+    width: 100%;
 }
 </style>
